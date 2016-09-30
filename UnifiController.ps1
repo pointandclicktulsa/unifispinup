8a1ebd18250539f663554be7cfa5acf798d3b8ca
colepnc <cole@pointandclicktulsa.com>
2016-09-30 14:19:37 -0500

    Documentation & better ISO path
    Added Documentation, logging, and testing for a different ISO path

--- UnifiController.ps1
+++ UnifiController.ps1
@@ -1,19 +1,40 @@
-﻿$VHDpath = "c:\Hyper-V\UnifiController.vhdx"
+﻿# Begin log file, this will be placed on the client the script is being run from, do not modify unless you want to disable logging
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path C:\UnifiController.txt -append
+
+# Script variables, change as needed
+$VHDpath = "c:\Hyper-V\UnifiController.vhdx"
 $ISO = "c:\admin\ISO\ubuntu-16.04.1-server-amd64.iso"
 $ISOPath = "c:\admin\ISO\"
 $VMName = "UnifiController"
 $ServerName = "Clark-PC"
 $VMSwitch = "External vSwitch"
 
-If (!(Test-Path $ISOpath)) 
+# Test for ISO folder existence
+If (!(Test-Path $ISOpath) -And !(Test-Path "C:\admin\ISOs\"))
 {
 New-Item -Path $ISOpath -ItemType Directory
 }
+else 
+{
+echo "Directory already exists!"
+}
+
+# Download Ubuntu ISO
 Invoke-WebRequest "http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-server-amd64.iso" -UseBasicParsing -OutFile "$ISO"
+
+# Create VHDX, VM, attach vSwitch, mount Ubuntu ISO
 New-VHD -Path $VHDpath -SizeBytes 10GB -Fixed
 New-VM -Name $VMName -MemoryStartupBytes 1024MB -Generation 1
 Add-VMHardDiskDrive -VMName $VMName -Path $VHDpath
 Set-VMDvdDrive -VMName $VMName -ControllerNumber 1 -Path $ISO
 Get-VMNetworkAdapter -VMName $VMName | Connect-VMNetworkAdapter -SwitchName $VMSwitch
+
+# Start and connect to VM
 Start-VM -Name UnifiController
 vmconnect $ServerName $VMName
+
+# End log file
+Stop-Transcript
