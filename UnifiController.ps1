98a31a8d63438d5f20b18a254c68cc3e0c191954
colepnc <cole@pointandclicktulsa.com>
2016-09-30 14:00:48 -0500

    added testing for admin folder
    checks if c:\admin\ISO\ exists and creates if needed.

--- UnifiController.ps1
+++ UnifiController.ps1
@@ -1,14 +1,19 @@
 ﻿$VHDpath = "c:\Hyper-V\UnifiController.vhdx"
-$ISOpath = "c:\admin\ISO\ubuntu-16.04.1-server-amd64.iso"
+$ISO = "c:\admin\ISO\ubuntu-16.04.1-server-amd64.iso"
+$ISOPath = "c:\admin\ISO\"
 $VMName = "UnifiController"
-$servername = "Clark-PC"
+$ServerName = "Clark-PC"
 $VMSwitch = "External vSwitch"
 
-Invoke-WebRequest "http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-server-amd64.iso" -UseBasicParsing -OutFile "$ISOpath"
+If (!(Test-Path $ISOpath)) 
+{
+New-Item -Path $ISOpath -ItemType Directory
+}
+Invoke-WebRequest "http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-server-amd64.iso" -UseBasicParsing -OutFile "$ISO"
 New-VHD -Path $VHDpath -SizeBytes 10GB -Fixed
 New-VM -Name $VMName -MemoryStartupBytes 1024MB -Generation 1
 Add-VMHardDiskDrive -VMName $VMName -Path $VHDpath
-Set-VMDvdDrive -VMName $VMName -ControllerNumber 1 -Path $ISOpath
+Set-VMDvdDrive -VMName $VMName -ControllerNumber 1 -Path $ISO
 Get-VMNetworkAdapter -VMName $VMName | Connect-VMNetworkAdapter -SwitchName $VMSwitch
 Start-VM -Name UnifiController
-vmconnect $servername $VMName
+vmconnect $ServerName $VMName
