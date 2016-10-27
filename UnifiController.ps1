7fc68fc95501307b6ba8014378eba31900053837
colepnc <cole@pointandclicktulsa.com>
2016-10-27 11:57:21 -0500

    Error checking, better VMSwitch
    Added automatic fetching of external vSwitches, and some error checking
    to attaching the vSwitch in case one does not exist. Also modified all {
    } to make formatting neater.

--- UnifiController.ps1
+++ UnifiController.ps1
@@ -10,26 +10,25 @@ $ISOPath = "c:\admin\ISO\"
 $VMName = "Unifi"
 $VHDpath = "c:\Hyper-V\$VMName.vhdx"
 $ServerName = "$env:computername"
-$VMSwitch = "Get-VMSwitch -SwitchType External"
+$VMSwitch = Get-VMSwitch -SwitchType External |
+              Select-Object -First 1 |
+              ForEach-Object Name
 
 # Test for ISO folder existence
-If (!(Test-Path $ISOpath) -And !(Test-Path "C:\admin\ISOs\"))
-{
+If (!(Test-Path $ISOpath) -And !(Test-Path "C:\admin\ISOs\")) {
 New-Item -Path $ISOpath -ItemType Directory
 }
-else 
-{
+else {
 echo "ISO directory already exists!"
 }
 
 # Download Ubuntu ISO
-If (!(Test-Path $ISO))
-{
+If (!(Test-Path $ISO)) {
+echo "Downloading Ubuntu Server 16.04.1 LTS ISO"
 Invoke-WebRequest "http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-server-amd64.iso" -UseBasicParsing -OutFile "$ISO"
 }
-else
-{
-echo "Ubuntu 16.04.1 ISO already exists!"
+else {
+echo "Ubuntu Server 16.04.1 LTS ISO already exists!"
 }
 
 # Create VHDX, VM, attach vSwitch, mount Ubuntu ISO
@@ -37,7 +36,10 @@ New-VHD -Path $VHDpath -SizeBytes 20GB -Fixed
 New-VM -Name $VMName -MemoryStartupBytes 512MB -Generation 1
 Add-VMHardDiskDrive -VMName $VMName -Path $VHDpath
 Set-VMDvdDrive -VMName $VMName -ControllerNumber 1 -Path $ISO
-Get-VMNetworkAdapter -VMName $VMName | Connect-VMNetworkAdapter -SwitchName ($VMSwitch)
+if ($VMSwitch -ne $null) {
+  Get-VMNetworkAdapter -VMName $VMName |
+    Connect-VMNetworkAdapter -SwitchName $VMSwitch
+}
 
 # Start and connect to VM
 Start-VM -Name $VMName
