189ff3ec6616f85392013d71b7cbbcdd25bfe8a9
colepnc <cole@pointandclicktulsa.com>
2017-03-31 10:29:05 -0500

    Ubuntu Update

--- UnifiController.ps1
+++ UnifiController.ps1
@@ -7,7 +7,7 @@ Start-Transcript -path C:\Unifi.txt -append
 # Script variables, change as needed
 # If you want to run this against a remote Hyper-V host, change $ServerName to a proper computer name.
 # If you have multiple External vSwitches you'll probably also have to manually input the name of the desired vSwitch in $VMSwitch
-$ISO = "c:\admin\ISO\ubuntu-16.04.1-server-amd64.iso"
+$ISO = "c:\admin\ISO\ubuntu-16.04.2-server-amd64.iso"
 $ISOPath = "c:\admin\ISO\"
 $VMName = "Unifi"
 $VHDpath = "c:\Hyper-V\$VMName.vhdx"
@@ -26,11 +26,11 @@ echo "ISO directory already exists!"
 
 # Download Ubuntu ISO
 If (!(Test-Path $ISO)) {
-echo "Downloading Ubuntu Server 16.04.1 LTS ISO"
-Invoke-WebRequest "http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-server-amd64.iso" -UseBasicParsing -OutFile "$ISO"
+echo "Downloading Ubuntu Server 16.04.2 LTS ISO"
+Invoke-WebRequest "http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-server-amd64.iso" -UseBasicParsing -OutFile "$ISO"
 }
 else {
-echo "Ubuntu Server 16.04.1 LTS ISO already exists!"
+echo "Ubuntu Server 16.04.2 LTS ISO already exists!"
 }
 
 # Create VHDX, VM, attach vSwitch, mount Ubuntu ISO
