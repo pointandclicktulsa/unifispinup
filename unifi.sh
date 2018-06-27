f46923dc9110009d6f66af2fd6e0d592eb5cbd1a
colepnc <cole@pointandclicktulsa.com>
2018-06-27 15:01:40 -0500

    UFW+Updates
    Updated to use UFW rather than IPTABLES, removed all the Hyper-V integration as it's baked into 18.04 (and so is htop)

--- unifi.sh
+++ unifi.sh
@@ -1,29 +1,25 @@
 # Running on port <1024 no longer works, controller runs as non-root since 5.6.22! Unifi MUST use a port above 1024
-# If you want the unifi controller to run the web interface on 443,
-# after the reboot, run "sudo sed -i "40i unifi.https.port=443" /usr/lib/unifi/data/system.properties"
-# If you want it on 8443, do nothing, other than change this script's iptables rules to alow 8443.
 # Set your static IP information in the variables below
-ip="192.168.1.22"
-netmask="255.255.255.0"
+ip="192.168.1.22/24"
 gateway="192.168.1.1"
 dns="192.168.1.2"
 # Start the script proper
-# Update Ubuntu before starting unifi install, and install cron-apt/htop for maintenance purposes
+# Update Ubuntu before starting unifi install, and install cron-apt for maintenance purposes
 apt-get update
 apt-get dist-upgrade -y
-apt-get install cron-apt htop linux-virtual-lts-xenial linux-tools-virtual-lts-xenial linux-cloud-tools-virtual-lts-xenial -y
+apt-get install cron-apt -y
 # Disable ipv6 on all interfaces, remove if your network is actually using ipv6
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
 sysctl -p
 # Set static IP, this may need changed if using ipv6
-sed -i -e 's/iface eth0 inet dhcp/#iface eth0 inet dhcp/g' /etc/network/interfaces
-echo "iface eth0 inet static" >> /etc/network/interfaces
-echo "address $ip" >> /etc/network/interfaces
-echo "netmask $netmask" >> /etc/network/interfaces
-echo "gateway $gateway" >> /etc/network/interfaces
-echo "dns-nameservers $dns" >> /etc/network/interfaces
+sed -i -e 's/dhcp4: yes/dhcp4: no/g' /etc/netplan/01-netcfg.yaml
+echo "      addresses: [$ip]" >> /etc/netplan/01-netcfg.yaml
+echo "      gateway4: $gateway" >> /etc/netplan/01-netcfg.yaml
+echo "      nameservers:" >> /etc/netplan/01-netcfg.yaml
+echo "       addresses: [$dns]" >> /etc/netplan/01-netcfg.yaml
+netplan apply
 # Add Unifi sources, if you want to install from a different channel than unifi5 modify this portion
 echo "## Debian/Ubuntu" >> /etc/apt/sources.list
 echo "# stable => unifi4" >> /etc/apt/sources.list
@@ -40,21 +36,14 @@ apt-get install unifi -y
 # not sure if smallfiles makes a big difference, testing needed
 # echo "unifi.db.extraargs=--smallfiles" >> /usr/lib/unifi/data/system.properties
 echo 'ENABLE_MONGODB=no' | sudo tee -a /etc/mongodb.conf > /dev/null
-# Iptables hardening to only allow the 7 ports you need
-iptables -F
-iptables -P INPUT DROP
-iptables -A INPUT -i lo -p all -j ACCEPT
-iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p udp -m udp --dport 3478 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 6789 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 8443 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 8843 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 8880 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 8080 -j ACCEPT
-iptables -A INPUT -p udp -m udp --dport 10001 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
-iptables -A INPUT -j DROP
-# Iptables persistance through reboot
-su -c "iptables-save > /etc/iptables.conf"
-sed -i "13i iptables-restore < /etc/iptables.conf" /etc/rc.local
+# Firewall config
+ufw allow ssh
+ufw allow 3478
+ufw allow 6789
+ufw allow 58443
+ufw allow 8843
+ufw allow 8880
+ufw allow 8080
+ufw allow 10001
+ufw --force enable
 reboot
